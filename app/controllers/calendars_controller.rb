3020e56240997c7e660b8294d0e3bf5829c465f3
taip315 <beaeaeat@gmail.com>
2020-12-19 00:56:29 +0900

    Revert "Issue6"
    This reverts commit ae167cd7fb8ddf171afec05b37347fa41791b0d2.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,14 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, plans: today_plans}
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, 
+        :wday =>wdays[
+        if Date.today.wday + x <= 6
+          Date.today.wday + x
+        else
+          Date.today.wday + x - 7
+        end  
+        ]}
       @week_days.push(days)
     end
 
