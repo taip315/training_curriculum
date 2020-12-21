c1c1efa1c1d301f4909fd944e6c2dd7570f01f8a
taip315 <beaeaeat@gmail.com>
2020-12-21 12:01:51 +0900

    シンボル表記、divタグ修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,8 +34,8 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, plans: today_plans, 
-        :wday =>wdays[
+      days = {month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, 
+        wday: wdays[
         if Date.today.wday + x <= 6
           Date.today.wday + x
         else
